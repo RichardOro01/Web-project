6764457e5eed96c23e1e2462d3ffe5bec8418057
Javy011021 <garciaj1246@gmail.com>
2023-09-21 18:51:04 -0400

    fix: inputs validate

--- frontend/scripts/driverValid.js
+++ frontend/scripts/driverValid.js
@@ -1,13 +1,54 @@
-{
+{    
     let forms = document.getElementsByClassName("form");
     for (let i = 0; i < forms.length; i++) {
         forms[i].addEventListener("submit", (event) => {
             const ID = document.getElementById("id").value;
-            const regex = /^(\d{6})([0-8])(\d{2})([0-9]|[13579])(\d)$/;
-            if (!regex.test(ID)) {
+            if (!CIvalidate.test(ID)) {
                 alert("CI incorrecto");
                 event.preventDefault();
             }
         });
     }
-}    
+}
+{
+    let name = document.getElementById("name");
+    name.addEventListener("keypress", function(event){
+        const charCode = event.charCode;
+        if(!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122))) {
+            event.preventDefault();
+        }else if (name.value.length > 35){
+            event.preventDefault();
+        }    
+    })
+}
+{
+    let id = document.getElementById("id");
+    id.addEventListener("keypress", function(event){
+        const charCode = event.charCode;
+        if(!(charCode >= 48 && charCode <= 57)){
+            event.preventDefault();
+        }else if (id.value.length > 11){
+            event.preventDefault();
+        }    
+    })
+}
+{
+    let phone = document.getElementById("phone");
+    phone.addEventListener("keypress", function(event){
+        const charCode = event.charCode;
+        if(!(charCode >= 48 && charCode <= 57)){
+            event.preventDefault();
+        }else if (phone.value.length > 8){
+            event.preventDefault();
+        }    
+    })
+}
+{
+    let address = document.getElementById("address");
+    address.addEventListener("keypress", function(event){
+        const charCode = event.charCode;
+        if (address.value.length > 35){
+            event.preventDefault();
+        }    
+    })
+}
