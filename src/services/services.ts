0db2243ce5c1dca9f2eb5070af500a1c2d4f79b2
Richard Oropesa <richardoro2001@gmail.com>
2023-10-24 14:15:14 -0400

    minor

--- src/services/services.ts
+++ src/services/services.ts
@@ -2,10 +2,9 @@ import { CRUD_ModalsType } from "@/components/modals";
 import brandService from "./tables/brands";
 import contractService from "./tables/contracts";
 import servicesService from "./tables/services";
-import discrepancyService from "./tables/discrepancies"
+import discrepancyService from "./tables/discrepancies";
 import roadmapService from "./tables/roadmaps";
 import { Service } from "./IService";
-
 
 const services: { [key in CRUD_ModalsType]: Service } = {
   brands: brandService,
