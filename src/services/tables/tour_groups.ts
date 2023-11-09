cbc65618b7ced739ee805e11cdc978bd2dc32242
Richard Oropesa <richardoro2001@gmail.com>
2023-11-09 12:56:05 -0500

    fix: sour group api url

--- src/services/tables/tour_groups.ts
+++ src/services/tables/tour_groups.ts
@@ -3,11 +3,11 @@ import { DELETE, GET, POST } from "..";
 import { Service } from "../IService";
 
 const tourService: Service = {
-  get: async (key?: string) => GET(`tourist_groups${key ? `/${key}` : ""}`),
-  add: async (data: Tourist) => POST("tourist_groups", data),
-  delete: async (key: string) => DELETE(`tourist_groups/${key}`),
+  get: async (key?: string) => GET(`tour_groups${key ? `/${key}` : ""}`),
+  add: async (data: Tourist) => POST("tour_groups", data),
+  delete: async (key: string) => DELETE(`tour_groups/${key}`),
   update: async (key: string, body: Tourist) =>
-    POST(`tourists_groups/${key}`, body),
+    POST(`tour_groups/${key}`, body),
 };
 
 export default tourService;
