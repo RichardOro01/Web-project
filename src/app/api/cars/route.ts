8af32a10184bd1bcd51b0beb41d1e07b002c36f8
Richard Oropesa <richardoro2001@gmail.com>
2023-11-30 00:10:12 -0500

    fix: car code from api

--- src/app/api/cars/route.ts
+++ src/app/api/cars/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import { deleteElementDB, readDB, writeDB } from "@/services/json";
 import { Car } from "@/interfaces/Car";
 import prisma from "@/lib/prisma";
 
@@ -11,13 +10,24 @@ export const GET = async () => {
   const brands = await prisma.brand.findMany();
   const drivers = await prisma.driver.findMany();
   const result: Car[] = cars.map((car) => ({
+    car_code: car.car_code,
     number: car.fleet_number,
     plate: car.plate,
     brand: brands.find((b) => b.brand_code === car.brand_code),
-    couple:{
-      couple_code : Number(couples.find((c) => c.couple_code === car.couple_code)?.couple_code),
-      driver1: drivers.find((d) => d.driver_code == couples.find((c) => c.couple_code === car.couple_code)?.driver_1),
-      driver2: drivers.find((d) => d.driver_code == couples.find((c) => c.couple_code === car.couple_code)?.driver_2),
+    couple: {
+      couple_code: Number(
+        couples.find((c) => c.couple_code === car.couple_code)?.couple_code
+      ),
+      driver1: drivers.find(
+        (d) =>
+          d.driver_code ==
+          couples.find((c) => c.couple_code === car.couple_code)?.driver_1
+      ),
+      driver2: drivers.find(
+        (d) =>
+          d.driver_code ==
+          couples.find((c) => c.couple_code === car.couple_code)?.driver_2
+      ),
     },
   }));
   return NextResponse.json(result ?? []);
@@ -29,7 +39,7 @@ export const POST = async (request: Request, response: Response) => {
     await prisma.car.create({ data });
     return NextResponse.json({ ok: true });
   } catch (error: any) {
-    console.log(error)
+    console.log(error);
     return NextResponse.json("Error creando carro", { status: 400 });
   }
 };
--- src/interfaces/Car.ts
+++ src/interfaces/Car.ts
@@ -2,18 +2,19 @@ import { Brand } from "./Brand";
 import { Couple } from "./Couple";
 
 export interface Car {
-    number: number;
-    plate: string|null;
-    brand?: Brand;
-    couple?: Couple;
-  }
+  car_code: number;
+  number: number;
+  plate: string | null;
+  brand?: Brand;
+  couple?: Couple;
+}
 
-  export interface EditCar extends Omit<Car,'couple' & 'brand'> {
-    fleet_number?:number;
-    couple_code?:number;
-    brand_code?:number;
-  }
-  
-  export interface CreateCar extends Omit<EditCar, 'number'> {
-    fleet_number?:number;
-  }
+export interface EditCar extends Omit<Car, "couple" | "brand"> {
+  fleet_number?: number;
+  couple_code?: number;
+  brand_code?: number;
+}
+
+export interface CreateCar extends Omit<EditCar, "number" | "car_code"> {
+  fleet_number?: number;
+}
