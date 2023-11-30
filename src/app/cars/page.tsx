7f2da26e8f8224ca0d6afc47954d111e246608a7
Richard Oropesa <richardoro2001@gmail.com>
2023-11-30 14:16:03 -0500

    feat: deleting flow

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,45 +1,51 @@
-import React from 'react'
+import React from "react";
 import { ColumnsType } from "antd/es/table";
-import { Car } from '@/interfaces/Car';
-import carService from '@/services/tables/cars';
-import TableData from '@/components/commons/tables/TableData';
-import { carAdapter } from '@/interfaces/adapters/CarAdapter';
+import { Car } from "@/interfaces/Car";
+import carService from "@/services/tables/cars";
+import TableData from "@/components/commons/tables/TableData";
+import { carAdapter } from "@/interfaces/adapters/CarAdapter";
 
 const columns: ColumnsType<Car> = [
-    {
-      title: "Number",
-      dataIndex: "number",
-      key: "number",
-    },
-    {
-      title: "Plate",
-      dataIndex: "plate",
-      key: "plate",
-    },
-    {
-      title: "Brand",
-      dataIndex: "brand_name",
-      key: "brand_name",
-    },
-    {
-      title: "Couple",
-      dataIndex: "couple_name",
-      key: "couple_name",
-    },
-  ];
+  {
+    title: "Fleet number",
+    dataIndex: "number",
+    key: "number",
+  },
+  {
+    title: "Plate",
+    dataIndex: "plate",
+    key: "plate",
+  },
+  {
+    title: "Brand",
+    dataIndex: "brand_name",
+    key: "brand_name",
+  },
+  {
+    title: "Couple",
+    dataIndex: "couple_name",
+    key: "couple_name",
+  },
+];
 
 const CarPage = async () => {
-    let cars: Car[] = [];
-    try {
-      cars = await carService.get();
-    } catch (error) {
-      console.log(error);
-    }
-    return (
-      <main className="flex flex-col gap-8 p-5">
-        <TableData title="Cars" modal="cars" dataToShow={carAdapter(cars)} data={cars} {...{ columns }} />
-      </main>
-    );
-}
+  let cars: Car[] = [];
+  try {
+    cars = await carService.get();
+  } catch (error) {
+    console.log(error);
+  }
+  return (
+    <main className="flex flex-col gap-8 p-5">
+      <TableData
+        title="Cars"
+        modal="cars"
+        dataToShow={carAdapter(cars)}
+        data={cars}
+        {...{ columns }}
+      />
+    </main>
+  );
+};
 
-export default CarPage
+export default CarPage;
