2ca01d0f3d4c8b896eb21fd41c5cc3c26ff9b2d1
Javy011021 <garciaj1246@gmail.com>
2024-01-02 09:34:31 -0500

    fix: topgroups

--- src/components/reports/Top3Groups.tsx
+++ src/components/reports/Top3Groups.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { top3Group } from "@/interfaces/Top3Group";
+import { Top3Group } from "@/interfaces/Top3Group";
 import { handleDownloadPDF } from "@/lib/utils";
 import { Button, Table } from "antd";
 import { ColumnsType } from "antd/es/table";
 import React from "react";
 
 interface Top3GroupsProps {
-  columns: ColumnsType<top3Group>;
-  data: top3Group[];
+  columns: ColumnsType<Top3Group>;
+  data: Top3Group[];
 }
 
 const Top3Groups: React.FC<Top3GroupsProps> = ({ columns, data }) => {
