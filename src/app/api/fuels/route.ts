2f8f2ac267ebc16ba83c1f5750732b550796a3a2
disciple2001 <dariohabana2001@gmail.com>
2024-01-02 19:08:40 -0500

    feat: added swagger files

--- src/app/api/fuels/route.ts
+++ src/app/api/fuels/route.ts
@@ -1,11 +1,31 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+/**
+ * @swagger
+ * /api/fuels:
+ *  get:
+ *    description: Returns the fuels
+ *    responses:
+ *      200:
+ *        description:
+ *      400:
+ *        description: Not found
+ */
 export const GET = async () => {
   const fuels = await prisma.fuel.findMany();
   return NextResponse.json(fuels);
 };
 
+/**
+ * @swagger
+ * /api/fuels:
+ *  post:
+ *    description: insert a new fuel
+ *    responses:
+ *      200:
+ *        description: Success
+ */
 export const POST = async (request: Request, response: Response) => {
   const data = await request.json();
   try {
