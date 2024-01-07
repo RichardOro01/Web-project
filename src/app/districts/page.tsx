a75fbd8b56039bd8b8315bcbf4baf22351649c0b
Javy011021 <garciaj1246@gmail.com>
2024-01-07 16:32:02 -0500

    feat: pages redirect

--- src/app/districts/page.tsx
+++ src/app/districts/page.tsx
@@ -3,7 +3,12 @@ import { District } from "@/interfaces/District";
 import { districtTableAdapter } from "@/interfaces/adapters/DistrictAdapter";
 import districtService from "@/services/tables/districts";
 import { ColumnsType } from "antd/es/table";
+import { getServerSession } from "next-auth";
 import React from "react";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+const roles = [1]
 
 const columns: ColumnsType<District> = [
   {
@@ -14,6 +19,11 @@ const columns: ColumnsType<District> = [
 ];
 
 const DistrictPage = async () => {
+  const session = await getServerSession(authOptions)
+  const rol = session?.role_code
+  if (rol&&!roles.includes(rol)) {
+    return redirect("/");
+  }
   let districts: District[] = [];
   try {
     districts = await districtService.get();
--- src/app/fuels/page.tsx
+++ src/app/fuels/page.tsx
@@ -3,7 +3,12 @@ import { Fuel } from "@/interfaces/Fuel";
 import { fuelTableAdapter } from "@/interfaces/adapters/FuelAdapter";
 import fuelService from "@/services/tables/fuels";
 import { ColumnsType } from "antd/es/table";
+import { getServerSession } from "next-auth";
 import React from "react";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+const roles = [1]
 
 const columns: ColumnsType<Fuel> = [
   {
@@ -14,6 +19,11 @@ const columns: ColumnsType<Fuel> = [
 ];
 
 const FuelPage = async () => {
+  const session = await getServerSession(authOptions)
+  const rol = session?.role_code
+  if (rol&&!roles.includes(rol)) {
+    return redirect("/");
+  }
   let fuels: Fuel[] = [];
   try {
     fuels = await fuelService.get();
