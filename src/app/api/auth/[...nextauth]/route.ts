cd112ef6a3e9571a312a90058e323730c773d8ea
Javy011021 <garciaj1246@gmail.com>
2024-01-07 22:27:54 -0500

    fix: export defaul

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -55,8 +55,7 @@ const authOptions = {
   },
 };
 
-const handler = NextAuth(authOptions);
 
-export default handler;
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST, authOptions };
