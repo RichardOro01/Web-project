53d73e23e667159237ed0e374939b8e6fbd4fdbf
Richard Oropesa <richardoro2001@gmail.com>
2024-01-09 01:02:26 -0500

    feat: main page responsive

--- src/components/main/OptionSection.tsx
+++ src/components/main/OptionSection.tsx
@@ -7,11 +7,14 @@ interface OptionSectionProps {
 
 const OptionSection: React.FC<OptionSectionProps> = ({ name, children }) => {
   return (
-    <section className="max-w-5xl w-full" id={name.toLowerCase()}>
+    <section
+      className="max-w-5xl w-full flex flex-col items-center md:items-start"
+      id={name.toLowerCase()}
+    >
       <h1 className="py-2 px-8 text-gray-500 rounded-xl mb-2 w-max shadow-xl text-2xl">
         {name}
       </h1>
-      <div className="flex flex-wrap justify-start gap-12 p-8 shadow-xl rounded-xl">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-12 p-8 shadow-xl rounded-xl w-10/12">
         {children}
       </div>
     </section>
